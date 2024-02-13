3d4ef70aaa09018c6ef1ad4b80cf5d17b3431621
ammiforu <ammiforu@gmail.com>
2024-02-13 16:56:19 -0600

    new project

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,2 @@
-import datetime
 print("Hello World today is my first day in python ")
 print("Date : Feb 13 2024 CST")
-Pday = datetime.date(2024,  2,  13)
-print(Pday)
-Tday = datetime.date.today()
-print(Tday)
-
-diff = Tday-Pday
-print(f'Days Past from starting :{diff.days} days')
